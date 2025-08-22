f0868ab8539c071a51e16a2af8645e7a7e9ae21b
nfskiy <strike2002.st@gmail.com>
2025-08-22 15:07:00 +0700

    вернул старую версию schemas

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,31 +1,32 @@
 # backend/app/schemas.py
 from pydantic import BaseModel, Field
 from typing import List, Any, Optional
-from pydantic.alias_generators import to_camel
+from pydantic.alias_generators import to_camel # НОВЫЙ ИМПОРТ
+
 
 class CafeSchema(BaseModel):
     id: str
     name: str
-    cover_image: Optional[str]
-    logo_image: Optional[str]
-    kitchen_categories: Optional[str]
-    rating: Optional[str]
-    cooking_time: Optional[str]
-    status: Optional[str]
-    opening_hours: Optional[str]
-    min_order_amount: Optional[int]
+    cover_image: Optional[str] = Field(None, alias='coverImage')
+    logo_image: Optional[str] = Field(None, alias='logoImage')
+    kitchen_categories: Optional[str] = Field(None, alias='kitchenCategories')
+    rating: Optional[str] = None
+    cooking_time: Optional[str] = Field(None, alias='cookingTime')
+    status: Optional[str] = None
+    opening_hours: Optional[str] = Field(None, alias='openingHours')
+    min_order_amount: Optional[int] = Field(None, alias='minOrderAmount')
 
     class Config:
         from_attributes = True
-        alias_generator = to_camel
-        populate_by_name = True  
+        alias_generator = to_camel # <--- ДОБАВЬТЕ ЭТУ СТРОКУ
+        populate_by_name = True
 
 class CategorySchema(BaseModel):
     id: str
     cafe_id: str
     icon: Optional[str]
     name: Optional[str]
-    background_color: Optional[str]
+    background_color: Optional[str] = Field(None, alias='backgroundColor')
 
     class Config:
         from_attributes = True
@@ -51,12 +52,12 @@ class MenuItemSchema(BaseModel):
         from_attributes = True
 
 class CafeSettingsSchema(BaseModel):
-    min_order_amount: int
+    min_order_amount: int = Field(alias='minOrderAmount')
 
     class Config:
-        from_attributes = True
         alias_generator = to_camel
         populate_by_name = True
+        from_attributes = True
 
 # --- Схемы для заказа (OrderRequest) ---
 class OrderItemCafeItem(BaseModel):
