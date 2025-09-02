d202f94696cd6352e706c41f89a6ea83f8d3f39a
nfskiy <strike2002.st@gmail.com>
2025-09-02 10:31:00 +0700

    доработали админ панель Добавление строки поиска

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -65,7 +65,7 @@ class GlobalProductAdmin(ModelView, model=GlobalProduct):
     icon = "fa-solid fa-pizza-slice"
     column_list = [GlobalProduct.id, GlobalProduct.name, GlobalProduct.category, GlobalProduct.is_popular]
     column_searchable_list = [GlobalProduct.name, GlobalProduct.description]
-    column_filters = [GlobalProduct.category_id, GlobalProduct.is_popular]
+    column_filters = [GlobalProduct.category, GlobalProduct.is_popular]
     form_args = {"image": {"base_path": UPLOAD_DIR, "url_prefix": "/media/"}}
     form_columns = [
         GlobalProduct.id, GlobalProduct.name, GlobalProduct.description,
@@ -128,7 +128,7 @@ class OrderAdmin(ModelView, model=Order):
             model = await self.get_obj(pk, session)
             if model:
                 model.status = "completed"
-        session.commit()
+        session.flush()
 
 class GlobalAddonGroupAdmin(ModelView, model=GlobalAddonGroup):
     category = "Меню"
